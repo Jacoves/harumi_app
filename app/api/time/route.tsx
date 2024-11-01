b6e8b949eafb5cd699fde8bd6843780f58392fc3
Jacoves <jacksontavaresrod@hotmail.com>
2024-11-01 20:49:04 -0300

    ajustes

--- app/api/time/route.tsx
+++ app/api/time/route.tsx
@@ -4,7 +4,6 @@ import {type NextRequest} from 'next/server'
 
 export async function GET(request: NextRequest) {
   const res = await fetch('https://api.kyroskoh.xyz/valorant/v1/mmr/br/Superaks/4773?show=combo&display=1')
-  const json = await res.text()
-  console.log(json)
-  return Response.json({response: res.body})
+  const text = await res.text()
+  return Response.json({response: text})
 }
