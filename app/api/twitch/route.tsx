af962e9309c5173864699d2ed71a8509dc011303
Jacoves <jacksontavaresrod@hotmail.com>
2024-11-02 01:26:38 -0300

    ajuste

--- app/api/twitch/route.tsx
+++ app/api/twitch/route.tsx
@@ -19,13 +19,11 @@ export async function GET(request: NextRequest) {
 export async function POST(request: NextRequest) {
   const webhook_url = `https://webhook.site/71be7601-2806-4a68-aacb-47bb788095e7`
   const response = await request.json()
-  const res = await fetch('https://api.kyroskoh.xyz/valorant/v1/mmr/br/Superaks/4773?show=combo&display=1')
-  const text = await res.text()
   const webhook = await fetch(webhook_url, {
     method: 'POST',
     body: JSON.stringify(
       response
     )
   })
-  return Response.json({original: response, text, webhook})
+  return Response.json(response)
 }
