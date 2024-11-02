9eb15dc52ffa2c863b0a6758022ae644c34650a1
Jacoves <jacksontavaresrod@hotmail.com>
2024-11-02 00:29:47 -0300

    ajustes

--- app/api/twitch/route.tsx
+++ app/api/twitch/route.tsx
@@ -3,14 +3,17 @@ import {type NextRequest} from 'next/server'
 
 export async function GET(request: NextRequest) {
   const webhook_url = `https://webhook.site/71be7601-2806-4a68-aacb-47bb788095e7`
-  request.json().then(console.log)
-  fetch(webhook_url, {
-    method: 'POST',
-    body: JSON.stringify(
-      {nome: 'ok'}
-    )
+  request.json().then(response => {
+    fetch(webhook_url, {
+      method: 'POST',
+      body: JSON.stringify(
+        response
+      )
+    })
   })
-  return Response.json(request.json())
+  return Response.json({
+    nome: 'ok'
+  })
 }
 
 export async function POST(request: NextRequest) {
