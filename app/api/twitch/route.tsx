2e73f70b1c122009a30f17f4837dedb4568374d7
Jacoves <jacksontavaresrod@hotmail.com>
2024-11-02 00:18:39 -0300

    ajuste

---
+++ app/api/twitch/route.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {type NextRequest} from 'next/server'
+
+export async function GET(request: NextRequest) {
+  const webhook_url = `https://webhook.site/71be7601-2806-4a68-aacb-47bb788095e7`
+  fetch(webhook_url, {
+    body: JSON.stringify(
+      request.json()
+    )
+  })
+  return Response.json(request.json())
+}
