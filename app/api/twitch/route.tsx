006a84e794da7e46483fa559777227e531c9499c
Jacoves <jacksontavaresrod@hotmail.com>
2024-11-02 00:21:20 -0300

    ajuste

--- app/api/twitch/route.tsx
+++ app/api/twitch/route.tsx
@@ -10,3 +10,13 @@ export async function GET(request: NextRequest) {
   })
   return Response.json(request.json())
 }
+
+export async function POST(request: NextRequest) {
+  const webhook_url = `https://webhook.site/71be7601-2806-4a68-aacb-47bb788095e7`
+  fetch(webhook_url, {
+    body: JSON.stringify(
+      request.json()
+    )
+  })
+  return Response.json(request.json())
+}
